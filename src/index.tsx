2acd3433f4f9189fb919656db72d9eda9d10117e
Amos Oruaroghene <inspiron.amos@gmail.com>
2025-07-31 16:00:54 +0100

    add support for custom transaction reference and metadata

--- src/index.tsx
+++ src/index.tsx
@@ -75,11 +75,10 @@ export const HydrogenCheckout = forwardRef(
       "isRecurring":${payload.isRecurring},
       "frequency":${payload.frequency},
       "CustomerName":"${payload.customerName || ''}",
-      "endDate": "${payload.endDate || ''}",
       "transactionRef": "${payload.transactionRef || ''}",
       "metaData": ${JSON.stringify(payload.metaData || [])},
     }
-
+      
     function onClose(e) {
       var response = {event:'close', e};
       window.ReactNativeWebView.postMessage(JSON.stringify(response))
